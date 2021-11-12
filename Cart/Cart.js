ab8bdf63a82f0caea957b79e648af01f9cfc8ff8
GirishGautam5 <girishkumarnarreddy@gmail.com>
2021-11-12 14:19:21 +0530

    item count added in Cartjs

--- Cart/Cart.js
+++ Cart/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export default function Cart() {
+    function itemsCount(){
+        return itemsCount
+    }
     return (
         <div>
             <h1>Cart JS file</h1>
             <p>Welcome to Cart Page</p>
+            <p>{itemsCount}</p>
         </div>
     )
 }
